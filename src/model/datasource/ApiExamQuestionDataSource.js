280ff61ffa940d418bc34230292cc98fb5ce3f3c
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-06-09 17:39:47 +0200

    Oppdater datasource og integrasjonstest

--- src/model/datasource/ApiExamQuestionDataSource.js
+++ src/model/datasource/ApiExamQuestionDataSource.js
@@ -2,24 +2,121 @@
 import ApiDataSource from "./ApiDataSource.js";
 
 export default class ApiExamQuestionDataSource extends ApiDataSource {
+	#subjectsPath = "/subjects";
+	#examsPath = "/exams";
+	#subjectExamsPathSuffix = "/exams";
+	#questionsPathSuffix = "/questions";
+	#practiceQuestionsQuery = "mode=practice";
+
 	async fetchAllExams() {
-		const subjects = await this.get("/subjects");
-		const examArrays = await Promise.all(
-			subjects.map((subject) => this.get(`/subjects/${encodeURIComponent(subject.id)}/exams`))
-		);
-		return examArrays.flat();
+		const subjects = await this.get(this.#subjectsPath);
+		const exams = [];
+
+		for (const subject of subjects) {
+			const subjectExams = await this.get(this.#buildSubjectExamsPath(subject.id));
+
+			for (const exam of subjectExams) {
+				exams.push(exam);
+			}
+		}
+
+		return exams;
 	}
 
 	async fetchExamById(examId) {
-		const [exam, questions] = await Promise.all([
-			this.get(`/exams/${encodeURIComponent(examId)}`),
-			this.get(`/exams/${encodeURIComponent(examId)}/questions?mode=practice`)
-		]);
-		return { ...exam, questions };
+		const exam = await this.get(this.#buildExamPath(examId));
+		const questions = await this.get(this.#buildExamQuestionsPath(examId));
+
+		return {
+			...exam,
+			questions: this.#toDomainQuestions(questions)
+		};
 	}
 
 	async fetchExamByBaseIdAndLang(baseId, language) {
 		const exams = await this.fetchAllExams();
-		return exams.find((exam) => exam.baseId === baseId && exam.lang === language) ?? null;
+
+		for (const exam of exams) {
+			if (exam.baseId === baseId && exam.lang === language) {
+				return exam;
+			}
+		}
+
+		return null;
+	}
+
+	#buildSubjectExamsPath(subjectId) {
+		return `${this.#subjectsPath}/${encodeURIComponent(subjectId)}${this.#subjectExamsPathSuffix}`;
+	}
+
+	#buildExamPath(examId) {
+		return `${this.#examsPath}/${encodeURIComponent(examId)}`;
+	}
+
+	#buildExamQuestionsPath(examId) {
+		return `${this.#buildExamPath(examId)}${this.#questionsPathSuffix}?${this.#practiceQuestionsQuery}`;
+	}
+
+	#toDomainQuestions(questions) {
+		const domainQuestions = [];
+
+		for (const question of questions) {
+			domainQuestions.push(this.#toDomainQuestion(question));
+		}
+
+		return domainQuestions;
+	}
+
+	#toDomainQuestion(question) {
+		return {
+			...question,
+			answers: this.#getAnswers(question),
+			options: this.#toDomainOptions(question.options)
+		};
+	}
+
+	#getAnswers(question) {
+		if (question.answers) {
+			return question.answers;
+		}
+
+		if (question.acceptedAnswers) {
+			return question.acceptedAnswers;
+		}
+
+		return [];
+	}
+
+	#toDomainOptions(options) {
+		if (!options) {
+			return options;
+		}
+
+		const domainOptions = [];
+
+		for (const option of options) {
+			domainOptions.push(this.#toDomainOption(option));
+		}
+
+		return domainOptions;
+	}
+
+	#toDomainOption(option) {
+		return {
+			...option,
+			correct: this.#getCorrectValue(option)
+		};
+	}
+
+	#getCorrectValue(option) {
+		if (option.correct !== undefined) {
+			return option.correct;
+		}
+
+		if (option.isCorrect !== undefined) {
+			return option.isCorrect;
+		}
+
+		return false;
 	}
 }
--- test/integration/examFlow.integration.test.js
+++ test/integration/examFlow.integration.test.js
@@ -2,239 +2,356 @@
 import { beforeAll, describe, expect, test } from "@jest/globals";
 import { QUESTION_TYPES } from "../../src/constants/QuestionTypes.js";
 
-const runApiIntegrationTests = process.env.RUN_API_INTEGRATION_TESTS === "true";
-const describeApiIntegration = runApiIntegrationTests ? describe : describe.skip;
+const shouldRunApiIntegrationTests = process.env.RUN_API_INTEGRATION_TESTS === "true";
+const examFlowApiDescribe = shouldRunApiIntegrationTests ? describe : describe.skip;
 
 class ExamFlowApiTestContext {
-    #calculateExamScoreUseCase;
-    #getAvailableExamsUseCase;
-    #getAvailableSubjectsUseCase;
-    #getExamByBaseIdAndLangUseCase;
-    #getExamQuestionsUseCase;
-    #getSubjectByIdUseCase;
-    #gradeAnswerUseCase;
+	calculateExamScoreUseCase = null;
+	getAvailableExamsUseCase = null;
+	getAvailableSubjectsUseCase = null;
+	getExamByBaseIdAndLangUseCase = null;
+	getExamQuestionsUseCase = null;
+	getSubjectByIdUseCase = null;
+	gradeAnswerUseCase = null;
 
-    async load() {
-        this.#configureEnvironment();
+	correctAnswerBuilder = new CorrectAnswerBuilder();
 
-        const dependencies = await import("../../src/di/dependencies.js");
+	async load() {
+		this.configureEnvironment();
 
-        this.#calculateExamScoreUseCase = dependencies.calculateExamScoreUseCase;
-        this.#getAvailableExamsUseCase = dependencies.getAvailableExamsUseCase;
-        this.#getAvailableSubjectsUseCase = dependencies.getAvailableSubjectsUseCase;
-        this.#getExamByBaseIdAndLangUseCase = dependencies.getExamByBaseIdAndLangUseCase;
-        this.#getExamQuestionsUseCase = dependencies.getExamQuestionsUseCase;
-        this.#getSubjectByIdUseCase = dependencies.getSubjectByIdUseCase;
-        this.#gradeAnswerUseCase = dependencies.gradeAnswerUseCase;
-    }
+		const dependencies = await import("../../src/di/dependencies.js");
 
-    get calculateExamScoreUseCase() {
-        return this.#requireLoaded(this.#calculateExamScoreUseCase, "calculateExamScoreUseCase");
-    }
+		this.calculateExamScoreUseCase = dependencies.calculateExamScoreUseCase;
+		this.getAvailableExamsUseCase = dependencies.getAvailableExamsUseCase;
+		this.getAvailableSubjectsUseCase = dependencies.getAvailableSubjectsUseCase;
+		this.getExamByBaseIdAndLangUseCase = dependencies.getExamByBaseIdAndLangUseCase;
+		this.getExamQuestionsUseCase = dependencies.getExamQuestionsUseCase;
+		this.getSubjectByIdUseCase = dependencies.getSubjectByIdUseCase;
+		this.gradeAnswerUseCase = dependencies.gradeAnswerUseCase;
+	}
 
-    get getAvailableExamsUseCase() {
-        return this.#requireLoaded(this.#getAvailableExamsUseCase, "getAvailableExamsUseCase");
-    }
+	configureEnvironment() {
+		process.env.VITE_API_BASE_URL = process.env.VITE_API_BASE_URL || "http://localhost:3000/api";
+		process.env.VITE_IMAGE_BASE_URL = process.env.VITE_IMAGE_BASE_URL || "http://localhost:3000";
+	}
 
-    get getAvailableSubjectsUseCase() {
-        return this.#requireLoaded(this.#getAvailableSubjectsUseCase, "getAvailableSubjectsUseCase");
-    }
+	async loadSubjects(language) {
+		const result = await this.getAvailableSubjectsUseCase.execute({ language });
+		return result.subjects;
+	}
 
-    get getExamByBaseIdAndLangUseCase() {
-        return this.#requireLoaded(this.#getExamByBaseIdAndLangUseCase, "getExamByBaseIdAndLangUseCase");
-    }
+	async loadExams(subjectId, language) {
+		return this.getAvailableExamsUseCase.execute({
+			subjectId,
+			language
+		});
+	}
 
-    get getExamQuestionsUseCase() {
-        return this.#requireLoaded(this.#getExamQuestionsUseCase, "getExamQuestionsUseCase");
-    }
+	async loadQuestions(examId) {
+		return this.getExamQuestionsUseCase.execute(examId);
+	}
 
-    get getSubjectByIdUseCase() {
-        return this.#requireLoaded(this.#getSubjectByIdUseCase, "getSubjectByIdUseCase");
-    }
+	async loadTranslatedExam(baseId, language) {
+		return this.getExamByBaseIdAndLangUseCase.execute({
+			baseId,
+			lang: language
+		});
+	}
 
-    get gradeAnswerUseCase() {
-        return this.#requireLoaded(this.#gradeAnswerUseCase, "gradeAnswerUseCase");
-    }
+	async loadSubject(subjectId, language) {
+		return this.getSubjectByIdUseCase.execute({
+			subjectId,
+			language
+		});
+	}
 
-    #configureEnvironment() {
-        process.env.VITE_API_BASE_URL ??= "http://localhost:3000/api";
-        process.env.VITE_IMAGE_BASE_URL ??= "http://localhost:3000";
-    }
+	calculateScore(questions, answersByQuestionId) {
+		return this.calculateExamScoreUseCase.execute(questions, answersByQuestionId);
+	}
 
-    #requireLoaded(value, name) {
-        if (!value) {
-            throw new Error(`ExamFlowApiTestContext was used before load(): ${name}`);
-        }
+	gradeAnswer(question, answer) {
+		return this.gradeAnswerUseCase.execute(question, answer);
+	}
 
-        return value;
-    }
+	getQuestionScore(question, answer) {
+		return this.gradeAnswerUseCase.getQuestionScore(question, answer);
+	}
+
+	buildCorrectAnswersByQuestionId(questions) {
+		const answersByQuestionId = {};
+
+		for (const question of questions) {
+			answersByQuestionId[question.id] = this.correctAnswerBuilder.build(question);
+		}
+
+		return answersByQuestionId;
+	}
+
+	buildCorrectAnswer(question) {
+		return this.correctAnswerBuilder.build(question);
+	}
 }
 
-function buildCorrectAnswer(question) {
-    if (question.type === QUESTION_TYPES.SINGLE) {
-        return question.options.findIndex((option) => option.correct);
-    }
+class CorrectAnswerBuilder {
+	build(question) {
+		if (question.type === QUESTION_TYPES.SINGLE) {
+			return this.buildSingleChoiceAnswer(question);
+		}
 
-    if (question.type === QUESTION_TYPES.MULTI) {
-        return question.options
-            .map((option, index) => option.correct ? index : null)
-            .filter((index) => index !== null);
-    }
+		if (question.type === QUESTION_TYPES.MULTI) {
+			return this.buildMultipleChoiceAnswer(question);
+		}
 
-    if (question.type === QUESTION_TYPES.FILL) {
-        return question.answers[0];
-    }
+		if (question.type === QUESTION_TYPES.FILL) {
+			return this.buildFillAnswer(question);
+		}
 
-    if (question.type === QUESTION_TYPES.MATRIX_PLACEMENT) {
-        return Object.fromEntries(
-            (question.items ?? []).map((item) => [item.id, item.correctQuadrantId ?? item.quadrantId])
-        );
-    }
+		if (question.type === QUESTION_TYPES.MATRIX_PLACEMENT) {
+			return this.buildMatrixPlacementAnswer(question);
+		}
 
-    return undefined;
+		return undefined;
+	}
+
+	buildSingleChoiceAnswer(question) {
+		const options = question.options || [];
+
+		for (let optionIndex = 0; optionIndex < options.length; optionIndex += 1) {
+			const option = options[optionIndex];
+
+			if (option.correct === true) {
+				return optionIndex;
+			}
+		}
+
+		return undefined;
+	}
+
+	buildMultipleChoiceAnswer(question) {
+		const correctOptionIndexes = [];
+		const options = question.options || [];
+
+		for (let optionIndex = 0; optionIndex < options.length; optionIndex += 1) {
+			const option = options[optionIndex];
+
+			if (option.correct === true) {
+				correctOptionIndexes.push(optionIndex);
+			}
+		}
+
+		return correctOptionIndexes;
+	}
+
+	buildFillAnswer(question) {
+		if (question.answers && question.answers.length > 0) {
+			return question.answers[0];
+		}
+
+		if (question.acceptedAnswers && question.acceptedAnswers.length > 0) {
+			return question.acceptedAnswers[0];
+		}
+
+		return undefined;
+	}
+
+	buildMatrixPlacementAnswer(question) {
+		const answerByItemId = {};
+		const items = question.items || [];
+
+		for (const item of items) {
+			answerByItemId[item.id] = item.correctQuadrantId || item.quadrantId;
+		}
+
+		return answerByItemId;
+	}
 }
 
-describeApiIntegration("exam flow API integration", () => {
-    const context = new ExamFlowApiTestContext();
+class ExamFlowApiTestAssertions {
+	findSubjectById(subjects, subjectId) {
+		for (const subject of subjects) {
+			if (subject.id === subjectId) {
+				return subject;
+			}
+		}
 
-    beforeAll(async () => {
-        await context.load();
-    });
+		return null;
+	}
 
-    test("loads visible subjects with exam counts from API", async () => {
-        const result = await context.getAvailableSubjectsUseCase.execute({ language: "no" });
+	findQuestionByType(questions, questionType) {
+		for (const question of questions) {
+			if (question.type === questionType) {
+				return question;
+			}
+		}
 
-        const in5431 = result.subjects.find((subject) => subject.id === "in5431");
+		return null;
+	}
 
-        expect(result.subjects.length).toBeGreaterThan(0);
-        expect(in5431).toMatchObject({
-            code: "IN5431",
-            examCount: 11,
-            isVisible: true
-        });
-    });
+	findQuestionByTitle(questions, questionTitle) {
+		for (const question of questions) {
+			if (question.title === questionTitle) {
+				return question;
+			}
+		}
 
-    test("loads available Norwegian exams for IN5431 from API", async () => {
-        const exams = await context.getAvailableExamsUseCase.execute({
-            subjectId: "in5431",
-            language: "no"
-        });
+		return null;
+	}
 
-        expect(exams).toHaveLength(11);
-        expect(exams.map((exam) => exam.id)).toEqual([
-            "mock-exam-1-no",
-            "mock-exam-2-no",
-            "mock-exam-3-no",
-            "mock-exam-4a-no",
-            "mock-exam-4b-no",
-            "mock-exam-5-no",
-            "mock-exam-definitions-no",
-            "mock-exam-drag-categorize-no",
-            "mock-exam-sustainability-no",
-            "mock-exam-digital-strategy-no",
-            "mock-exam-digital-transformation-no",
-        ]);
-        expect(exams.every((exam) => exam.questionCount > 0)).toBe(true);
-    });
+	expectExamIds(exams, expectedExamIds) {
+		const actualExamIds = [];
 
-    test("loads available English exams for IN5431 from API", async () => {
-        const exams = await context.getAvailableExamsUseCase.execute({
-            subjectId: "in5431",
-            language: "en"
-        });
+		for (const exam of exams) {
+			actualExamIds.push(exam.id);
+		}
 
-        expect(exams).toHaveLength(11);
-        expect(exams.map((exam) => exam.id)).toEqual([
-            "mock-exam-1-en",
-            "mock-exam-2-en",
-            "mock-exam-3-en",
-            "mock-exam-4a-en",
-            "mock-exam-4b-en",
-            "mock-exam-5-en",
-            "mock-exam-definitions-en",
-            "mock-exam-drag-categorize-en",
-            "mock-exam-sustainability-en",
-            "mock-exam-digital-strategy-en",
-            "mock-exam-digital-transformation-en",
-        ]);
-        expect(exams.every((exam) => exam.questionCount > 0)).toBe(true);
-    });
+		expect(actualExamIds).toEqual(expectedExamIds);
+	}
 
-    test("loads questions and calculates full score when all answers are correct", async () => {
-        const questions = await context.getExamQuestionsUseCase.execute("mock-exam-1-no");
-        const answers = Object.fromEntries(
-            questions.map((question) => [question.id, buildCorrectAnswer(question)])
-        );
+	expectEveryExamHasQuestions(exams) {
+		for (const exam of exams) {
+			expect(exam.questionCount).toBeGreaterThan(0);
+		}
+	}
+}
 
-        const result = context.calculateExamScoreUseCase.execute(questions, answers);
+examFlowApiDescribe("exam flow API integration", () => {
+	const context = new ExamFlowApiTestContext();
+	const assertions = new ExamFlowApiTestAssertions();
 
-        expect(questions).toHaveLength(25);
-        expect(result).toEqual({
-            score: 25,
-            totalPoints: 25,
-            percentage: 100
-        });
-    });
+	beforeAll(async () => {
+		await context.load();
+	});
 
-    test("grades actual question examples from API", async () => {
-        const questions = await context.getExamQuestionsUseCase.execute("mock-exam-1-en");
-        const fillQuestion = questions.find((question) => question.type === QUESTION_TYPES.FILL);
-        const multiQuestion = questions.find((question) => question.type === QUESTION_TYPES.MULTI);
-        const singleQuestion = questions.find((question) => question.type === QUESTION_TYPES.SINGLE);
+	test("loads visible subjects with exam counts from API", async () => {
+		const subjects = await context.loadSubjects("no");
+		const in5431Subject = assertions.findSubjectById(subjects, "in5431");
 
-        expect(context.gradeAnswerUseCase.execute(fillQuestion, buildCorrectAnswer(fillQuestion))).toBe(true);
-        expect(context.gradeAnswerUseCase.execute(multiQuestion, buildCorrectAnswer(multiQuestion))).toBe(true);
-        expect(context.gradeAnswerUseCase.execute(singleQuestion, buildCorrectAnswer(singleQuestion))).toBe(true);
-    });
+		expect(subjects.length).toBeGreaterThan(0);
+		expect(in5431Subject).toMatchObject({
+			code: "IN5431",
+			examCount: 12,
+			isVisible: true
+		});
+	});
 
-    test("grades Autonomy and alignment matrix questions from API", async () => {
-        const examIds = [
-            "mock-exam-drag-categorize-no",
-            "mock-exam-drag-categorize-en"
-        ];
+	test("loads available Norwegian exams for IN5431 from API", async () => {
+		const exams = await context.loadExams("in5431", "no");
 
-        for (const examId of examIds) {
-            const questions = await context.getExamQuestionsUseCase.execute(examId);
-            const question = questions.find((candidate) => candidate.title === "Autonomy and alignment");
+		expect(exams).toHaveLength(12);
 
-            expect(question).toMatchObject({
-                type: QUESTION_TYPES.MATRIX_PLACEMENT,
-                points: 4
-            });
-            expect(question.moduleId).toEqual(expect.any(String));
-            expect(question.groupId).toEqual(expect.any(String));
-            expect(question.items).toHaveLength(4);
+		assertions.expectExamIds(exams, [
+			"in5431-demo-no",
+			"mock-exam-1-no",
+			"mock-exam-2-no",
+			"mock-exam-3-no",
+			"mock-exam-4a-no",
+			"mock-exam-4b-no",
+			"mock-exam-5-no",
+			"mock-exam-definitions-no",
+			"mock-exam-drag-categorize-no",
+			"mock-exam-sustainability-no",
+			"mock-exam-digital-strategy-no",
+			"mock-exam-digital-transformation-no"
+		]);
 
-            const answer = buildCorrectAnswer(question);
+		assertions.expectEveryExamHasQuestions(exams);
+	});
 
-            expect(context.gradeAnswerUseCase.execute(question, answer)).toBe(true);
-            expect(context.gradeAnswerUseCase.getQuestionScore(question, answer)).toBe(4);
-        }
-    });
+	test("loads available English exams for IN5431 from API", async () => {
+		const exams = await context.loadExams("in5431", "en");
 
-    test("finds translated exam by base id and lang", async () => {
-        const exam = await context.getExamByBaseIdAndLangUseCase.execute({
-            baseId: "mock-exam-1",
-            lang: "en"
-        });
+		expect(exams).toHaveLength(11);
 
-        expect(exam).toMatchObject({
-            id: "mock-exam-1-en",
-            baseId: "mock-exam-1",
-            lang: "en"
-        });
-    });
+		assertions.expectExamIds(exams, [
+			"mock-exam-1-en",
+			"mock-exam-2-en",
+			"mock-exam-3-en",
+			"mock-exam-4a-en",
+			"mock-exam-4b-en",
+			"mock-exam-5-en",
+			"mock-exam-definitions-en",
+			"mock-exam-drag-categorize-en",
+			"mock-exam-sustainability-en",
+			"mock-exam-digital-strategy-en",
+			"mock-exam-digital-transformation-en"
+		]);
 
-    test("finds subject by id with exam count", async () => {
-        const subject = await context.getSubjectByIdUseCase.execute({
-            subjectId: "in5431",
-            language: "en"
-        });
+		assertions.expectEveryExamHasQuestions(exams);
+	});
 
-        expect(subject).toMatchObject({
-            id: "in5431",
-            code: "IN5431",
-            examCount: 11
-        });
-    });
+	test("loads questions and calculates full score when all answers are correct", async () => {
+		const questions = await context.loadQuestions("mock-exam-1-no");
+		const answersByQuestionId = context.buildCorrectAnswersByQuestionId(questions);
+
+		const result = context.calculateScore(questions, answersByQuestionId);
+
+		expect(questions).toHaveLength(25);
+		expect(result).toEqual({
+			score: 25,
+			totalPoints: 25,
+			percentage: 100
+		});
+	});
+
+	test("grades actual question examples from API", async () => {
+		const questions = await context.loadQuestions("mock-exam-1-en");
+
+		const fillQuestion = assertions.findQuestionByType(questions, QUESTION_TYPES.FILL);
+		const multipleChoiceQuestion = assertions.findQuestionByType(questions, QUESTION_TYPES.MULTI);
+		const singleChoiceQuestion = assertions.findQuestionByType(questions, QUESTION_TYPES.SINGLE);
+
+		const fillAnswer = context.buildCorrectAnswer(fillQuestion);
+		const multipleChoiceAnswer = context.buildCorrectAnswer(multipleChoiceQuestion);
+		const singleChoiceAnswer = context.buildCorrectAnswer(singleChoiceQuestion);
+
+		expect(context.gradeAnswer(fillQuestion, fillAnswer)).toBe(true);
+		expect(context.gradeAnswer(multipleChoiceQuestion, multipleChoiceAnswer)).toBe(true);
+		expect(context.gradeAnswer(singleChoiceQuestion, singleChoiceAnswer)).toBe(true);
+	});
+
+	test("grades Autonomy and alignment matrix questions from API", async () => {
+		const examIds = [
+			"mock-exam-drag-categorize-no",
+			"mock-exam-drag-categorize-en"
+		];
+
+		for (const examId of examIds) {
+			const questions = await context.loadQuestions(examId);
+			const question = assertions.findQuestionByTitle(questions, "Autonomy and alignment");
+
+			expect(question).toMatchObject({
+				type: QUESTION_TYPES.MATRIX_PLACEMENT,
+				points: 4
+			});
+
+			expect(question.moduleId).toEqual(expect.any(String));
+			expect(question.groupId).toEqual(expect.any(String));
+			expect(question.items).toHaveLength(4);
+
+			const answer = context.buildCorrectAnswer(question);
+
+			expect(context.gradeAnswer(question, answer)).toBe(true);
+			expect(context.getQuestionScore(question, answer)).toBe(4);
+		}
+	});
+
+	test("finds translated exam by base id and lang", async () => {
+		const exam = await context.loadTranslatedExam("mock-exam-1", "en");
+
+		expect(exam).toMatchObject({
+			id: "mock-exam-1-en",
+			baseId: "mock-exam-1",
+			lang: "en"
+		});
+	});
+
+	test("finds subject by id with exam count", async () => {
+		const subject = await context.loadSubject("in5431", "en");
+
+		expect(subject).toMatchObject({
+			id: "in5431",
+			code: "IN5431",
+			examCount: 11
+		});
+	});
 });
